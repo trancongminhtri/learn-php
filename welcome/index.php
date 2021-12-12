05dc5a3f08f968edb2d07204ae838e66e3ec27b8
MinhTri/trancongminhtri@gmail.com <trancongminhtri@gmail.com>
2021-12-12 18:31:12 +0700

    create file welcome

---
+++ welcome/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài học đầu tiền PHP</title>
+</head>
+<body>
+    <p>
+        Học lập trình Unicode
+    </p>
+
+    <?php 
+        echo '<p>Học Lập Trình Unicode</p>'
+    ?>
+</body>
+</html>
